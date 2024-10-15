aa832389ae5471d8d10efddaf6b24440c621bf49
adarshnarayan04 <adarshnarayangumla@gmail.com>
2024-10-15 21:46:48 +0530

    Update favicon link in index.html and cors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://jobportal-25u3.onrender.com', 
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
